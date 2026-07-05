070cbbb29d02de31bb548b3944ec04692ac052a2
leonascido <leonascimento5347@gmail.com>
2026-07-05 12:08:58 -0300

    bugfix(alrme rpm):adicionado alarme no simulador.py

--- src/simulador.py
+++ src/simulador.py
@@ -3,6 +3,7 @@ import random
 
 # Importem as funções de alarme aqui em baixo
 import src.alarme_velocidade as ver_velocidade
+import src.alarmerpm as aviso_RPM
 # ===========================================
 
 
@@ -46,6 +47,7 @@ def exibir_painel(rpm, velocidade, temp_cvt, bateria):
 
     # Coloquem os alarmes aqui em baixo
     ver_velocidade(velocidade)
+    aviso_RPM(rpm)
     # ===================================
 
     print("Aperte Ctrl+C para sair.\n")
